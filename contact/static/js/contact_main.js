b139bd94c5d7b1d58bb0ad8ad6cfcd3e662eebff
Isak-Landin <utbisalan@gmail.com>
2023-11-03 23:50:17 +0100

    Structural change, js, js-compatility changes

--- contact/static/js/contact_main.js
+++ contact/static/js/contact_main.js
@@ -1,15 +1,12 @@
-function showMore(btn) {
-  const personComponent = btn.closest('.person');
-  const arrow = personComponent.querySelector('.show-more-div');
+function showMore(show_more_div) {
+  const personComponent = show_more_div.closest('.person');
   const more = personComponent.querySelector('.more');
 
-  const isClosed = more.style.display == "none";
+  const isClosed = more.style.display != "block";
 
   if(isClosed){
-    arrow.style.display = "rotate(-135deg)";
     more.style.display = "block";
   } else {
-    arrow.style.display = "rotate(45deg)";
     more.style.display = "none";
   }
 }
